7568e307fa73bf593a193953c9486e74311dc85f
Ferdousfrd <ferdous.frd81@gmail.com>
2024-11-04 01:15:31 +0200

    finished the tenzis app. Want to update it more by addind dice emojies or dots later miself

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,80 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Die from "./components/Die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 import "./App.css"
 
 
 export default function App() {
-/**
- * Challenge: Add conditional styling to the Die component
- * so that if it's held (isHeld === true), its background color
- * changes to a light green (#59E391)
- * 
- * Remember: currently the Die component has no way of knowing
- * if it's "held" or not.
- */
 
-    const [dice, setDice] = useState(allNewDice())  // sets initial dice state to the array of obj returned by allNewDice
-    
-    function allNewDice() {                         // returns an array of 10 obj
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            newDice.push({                          // creating diec obj
-              value : Math.ceil(Math.random() * 6),
-              isHeld : true,
-              id : nanoid()
-            })
-        }
-        return newDice
+  const [dice, setDice] = useState(allNewDice())  // sets initial dice state to the array of obj returned by allNewDice
+  const [tenzies, setTenzies] = useState(false)   // state of the game is won
+
+
+  function generateNewDie(){                      // making an obj of die with random value and id
+    return {
+      value : Math.ceil(Math.random() * 6),
+      isHeld : false,
+      id : nanoid()
     }
-    
-    function rollDice() {                           // setDice calles the allNewDice func again and 
-        setDice(allNewDice())                       // gets a new array with new 10 nums and sets that to new dice state
+  }
+  
+  function allNewDice() {                         // returns an array of 10 obj
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(generateNewDie())
     }
-    
-    function holdDice(id){
-      console.log(id)
-      const currentDiceId = id
-      for(let i = 0; i<dice.length; i++){
-        if(dice[i].id === id){
-          console.log(dice[i].value)
-        }
-      }
+    return newDice
+  }
+  
+  function rollDice() {                                 
+    if(!tenzies){                            // if game not won,setting a new state to our dice array. after holding a dice with onClick
+      setDice(prev => prev.map(item => {                  // we dont change the obj which isHeld true
+        return item.isHeld ? item : generateNewDie()      // we just change the value on which isHeld is false
+      }))  
+    }   
+    else {                                  // if won
+      setDice(allNewDice())
+      setTenzies(false)
+    }                 
+  }
+  
+  function holdDice(id){
+    setDice(prev => prev.map(item =>{             // updateing our dice obj in array on click by toggling between isHeld true and false
+      return item.id === id ? {
+        ...item,
+        isHeld: !item.isHeld
+      } : item
+    }))
+  }
+
+  useEffect(()=> {
+    // if all dices are held
+    const allDieHeld = dice.every(die => die.isHeld)
+    // if all dices values are same
+    const firsDieValue = dice[0].value
+    const allDieSameValue = dice.every(die => die.value === firsDieValue)
+
+    if(allDieHeld && allDieSameValue){
+      setTenzies(true)
+      console.log("Won")
     }
 
-    const diceElements = dice.map(die => <Die key={die.id} handler={()=>holdDice(die.id)} value={die.value} isHeld={die.isHeld} />)
-    
-    return (
-        <main>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
-        </main>
-    )
+  },[dice])
+
+  const diceElements = dice.map(die => <Die key={die.id} handler={()=>holdDice(die.id)} value={die.value} isHeld={die.isHeld} />)
+  
+  return (
+    <main>
+        {tenzies && <Confetti />}
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        
+        <div className="dice-container">
+            {diceElements}
+        </div>
+
+        <button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+    </main>
+  )
 }
